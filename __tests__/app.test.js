dc540d7b668adfe6d916e17825892adebf2f877d
Charlie Hengemihle <charliehengemihle@gmail.com>
2022-11-01 18:04:43 -0600

    horoscopes testing and displaying

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 const { zodiacs } = require('../lib/zodiac-data');
+const { horoscopes } = require('../lib/horoscope-data');
 
 describe('zodiac routes', () => {
   it('/zodiac should return a list of signs', async () => {
@@ -14,9 +15,25 @@ describe('zodiac routes', () => {
 
   it('/zodiac/:id should return zodiac detail', async () => {
     const res = await request(app).get('/zodiac/1');
-    const gemini = {
+    const aquarius = {
       id: '1',
       name: 'aquarius',
+      dates: 'Jan 21 - Feb 19',
+      symbol: 'Water Bearer',
+    };
+    expect(res.body).toEqual(aquarius);
+  });
+});
+
+describe('horoscope route', () => {
+  it('/horoscope/:name should return a horoscope for the given sign', async () => {
+    const res = await request(app).get('/horoscopes/gemini');
+    const gemini = {
+      id: '5',
+      name: 'gemini',
+      dates: 'May 21 - Jun 20',
+      symbol: 'Twins',
+      horoscope: 'Look out behind you!',
     };
     expect(res.body).toEqual(gemini);
   });
